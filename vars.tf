34e54e1f4f060f197e474350b9c7d62c28a9ea75
vishant07 <sharma.vishant07@gmail.com>
2018-01-27 18:35:39 +0530

    let me test it first the i'll explain

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {} {
+    default = "us-east-1"
+}
+variable "AMIS" {
+    type = "map"
+    default = {
+        us-east-1 = "ami-66506c1c"
+
+    }
+}
